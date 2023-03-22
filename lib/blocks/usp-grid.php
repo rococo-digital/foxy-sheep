66c971548d19078d0053bfecf3af1066dddb8243
Matt Jones <matt@rococodigital.co.uk>
2023-03-22 13:15:38 +0000

    USP block responsive columns

--- lib/blocks/usp-grid.php
+++ lib/blocks/usp-grid.php
@@ -9,10 +9,10 @@ $icon_cards = get_field('icon_cards');
 ?>
 
 <section class="section usp-grid bg--green400">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row justify-content-center">
             <?php $i=1; foreach($icon_cards as $icon_card){?>
-                <div class="col-md-4 col-xxl-3 text-center" data-aos="fade-up" data-aos-delay="<?php echo $i * 100;?>">
+                <div class="col-sm col-6 text-center" data-aos="fade-up" data-aos-delay="<?php echo $i * 100;?>">
                    <div class="usp-grid__item">
                        <img src="<?php echo $icon_card['icon']['url'];?>" alt="<?php echo $icon_card['icon']['alt'];?>">
                    </div>
